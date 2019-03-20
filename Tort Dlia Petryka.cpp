41b80637fdf8b827c05e5ff18f468acefe039c00
marii-pavlo <43071980+pawlomarij1@users.noreply.github.com>
2019-03-20 17:14:28 +0200

    Add files via upload

---
+++ Tort Dlia Petryka.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <cmath>
+#define PI 3.141592
+
+using namespace std;
+
+int main() 
+{
+	int N;
+	int R;
+	cin >> N >> R;
+
+	double pl = PI*R*R;
+	pl/=N;
+	double res;
+	res = sqrt(pl/PI);
+	cout << res;
+
+
+	getchar();
+	getchar();
+	return 0;
+}
