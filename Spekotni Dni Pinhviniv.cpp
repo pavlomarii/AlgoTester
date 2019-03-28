839f6eccd6380504cd1d0d6ee4eaa96b56135fd6
marii-pavlo <43071980+pawlomarij1@users.noreply.github.com>
2019-03-28 19:48:27 +0200

    Add files via upload

---
+++ Spekotni Dni Pinhviniv.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	long L;
+	long W;
+	long U;
+	long D;
+	cin >> L >> W >> U >> D;
+
+	if(L > W || L > U+D)
+		cout << "Forget about the cocktails, man!";
+	else
+		cout << "Three times Sex on the Beach, please!";
+
+
+
+
+
+
+	getchar();
+	getchar();
+	return 0;
+}
